ded89ee66e8e13b1487a8671046da7ffca748dba
Nathan Clark <nwclark@gmail.com>
2021-12-27 19:45:06 -0800

    Initial Commit

---
+++ VizSort/VizSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  VizSortApp.swift
+//  VizSort
+//
+//  Created by Nathan Clark on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct VizSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
